75c452dffa3fa7329980b5ecf95e35089b88f4b7
samuquisdoff <samuquisdoff2@gmail.com>
2026-03-01 16:04:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,10 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-// ✅ Porta obrigatória Railway
+app.get("/", (req, res) => {
+  res.send("Servidor online 🔥");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
